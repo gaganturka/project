45c64102c94cdd9412d192c70a6a1e8a2d1a5758
seraphic <seraphic@seraphics-MacBook-Air-2.local>
2022-04-20 19:00:31 +0530

    rks

--- server/db.js
+++ server/db.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/borhanbackend";
+const mongoURI =
+  "mongodb+srv://borhan:borhan@cluster0.whgyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+// const mongoURI = "mongodb://localhost:27017/borhanbackend";
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
     console.log("connection successful to mongoose");
