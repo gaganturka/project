c4981bef67fe7976564865343a83deed18da1851
AK <ak@example.com>
2022-10-07 13:05:25 +0530

    more firm modules and merge with dev branch

--- models/Expert_User.js
+++ models/Expert_User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const {Schema} = mongoose;
 const APP_CONSTANTS = require("../appConstants");
+const UserPlans = require("../models/paymentGateway/userPlansBought");
 
 const ExpertUserSchema = new Schema(
     {
